c4e362a00bb0fcd32f2472a627b67092d1c041f1
GeorgeRoblox <w.jirka12@gmail.com>
2026-06-07 16:01:26 +0200

    Update Base.lua

--- Components/Base.lua
+++ Components/Base.lua
@@ -1,1 +1,27 @@
+-- LunithLib/Components/Base.lua
+local Base = {}
+Base.__index = Base
 
+function Base.new(library, section)
+    local self = setmetatable({}, Base)
+    self.Library = library
+    self.Section = section
+    self.Flag = nil
+    self.Callback = function() end
+    return self
+end
+
+function Base:SetFlag(flag)
+    self.Flag = flag
+    if flag then
+        self.Library.Flags[flag] = self.Library.Flags[flag] or nil
+    end
+end
+
+function Base:SetCallback(cb)
+    if type(cb) == "function" then
+        self.Callback = cb
+    end
+end
+
+return Base
